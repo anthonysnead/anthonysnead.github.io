db7bbf6f3371aad28cc8aef4c9e4218540095555
anthonysnead <51483548+anthonysnead@users.noreply.github.com>
2026-02-28 14:41:49 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 
 // IMPORTANT: Replace with your NEW rotated Mapbox token
-const MAPBOX_TOKEN = "pk.YOUR_NEW_TOKEN_HERE";
+const MAPBOX_TOKEN = "pk.eyJ1IjoiYXNuZWFkIiwiYSI6ImNtbTZuemRnNTBsb3YycG91ZGdkYjF0ZncifQ.wRVnWQ1DEkLhjapEk4Pwdg";
